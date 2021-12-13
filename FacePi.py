4eae9f568ef2f8df711aaed73078955a4762158e
Matcha_Slime <caixianzhang2003@gmail.com>
2021-12-13 11:05:54 +0800

    新增本地圖檔辨識

--- FacePi.py
+++ FacePi.py
@@ -1,10 +1,42 @@
-import fire, json
+import fire, os, json
 import http.client, urllib.request, urllib.parse, urllib.error, base64
 basepath = os.path.dirname(os.path.realpath(__file__))
 headers = os.path.join(basepath,'Config.json')
 
 class FacePi:
-    def detectImageUrl(self. imageurl):
+    def detectLocalImageUrl(self, imagepath):
+        headers = {
+            # Request headers
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': self.readConfig()['api_key'],
+        }
+
+        params = urllib.parse.urlencode({
+            # Request parameters
+            'returnFaceId': 'true',
+            'returnFaceLandmarks': 'false',
+            'returnFaceAttributes': 'age,gender',
+            #'recognitionModel': 'recognition_04',
+            'returnRecognitionModel': 'false',
+            'detectionModel': 'detection_01',
+            'faceIdTimeToLive': '86400',
+        })
+
+        print('imageurl=', imagepath)
+        requestbody = open(imagepath, "rb").read()
+        try:
+            conn = http.client.HTTPSConnection(self.readConfig()['host'])
+            conn.request("POST", "/face/v1.0/detect?%s" % params, requestbody, headers)
+            response = conn.getresponse()
+            data = response.read()
+            json_face_detect = json.load(str(data, 'UTF-8'))
+            print("detectLocalImage:",f"{imagepath}偵測到{len(json_face_detect)}個人")
+            return json_face_detect
+        
+        except Exception as e:
+            print("[Errno {0}]連線檢查失敗！請檢察網路設定。 {1}".format(e.errno, e.strerror))
+
+    def detectImageUrl(self, imageurl):
         headers = {
             # Request headers
             'Content-Type': 'application/json',
@@ -15,7 +47,7 @@ class FacePi:
             # Request parameters
             'returnFaceId': 'true',
             'returnFaceLandmarks': 'false',
-            'returnFaceAttributes': 'age,gender'
+            'returnFaceAttributes': 'age,gender',
             #'recognitionModel': 'recognition_04',
             'returnRecognitionModel': 'false',
             'detectionModel': 'detection_01',
@@ -32,6 +64,7 @@ class FacePi:
             json_face_detect = json.load(str(data, 'UTF-8'))
             print("detectLocalImage:",f"{imageurl}偵測到{len(json_face_detect)}個人")
             return json_face_detect
+        
         except Exception as e:
             print("[Errno {0}]連線檢查失敗！請檢察網路設定。 {1}".format(e.errno, e.strerror))
 
